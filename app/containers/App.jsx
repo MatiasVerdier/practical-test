78661da0680063c4836cca589ebfff42a519b7bf
Matias Verdier <verdier.matias@gmail.com>
2016-04-28 20:32:28 -0300

    include component blocks in App component

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import Header from '../components/Header'
+import TextBlock from '../components/TextBlock'
+import TwitterBlock from '../components/TwitterBlock'
+import CarrouselBlock from '../components/CarrouselBlock'
+import ShareBlock from '../components/ShareBlock'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <h1>Hola Mundo</h1>
+        <Header />
+        <TextBlock />
+        <TwitterBlock />
+        <CarrouselBlock />
+        <ShareBlock />
       </div>
     )
   }
