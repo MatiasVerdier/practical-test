a1180f3d6b54f5ea6fa2829aff14ea9b5117a422
Matias Verdier <verdier.matias@gmail.com>
2016-04-29 17:43:04 -0300

    add menu icon and toggle handler

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 
 class Navigation extends Component {
+  constructor (props) {
+    super(props)
+    this.toggleMenu.bind(this)
+  }
+
   render () {
     return (
-      <nav className='navigation'>
-        <a href='#text' className='item'>Text</a>
-        <a href='#twitter' className='item'>Twitter</a>
-        <a href='#carrousel' className='item'>Carrousel</a>
-        <a href='#share' className='item'>Share</a>
-      </nav>
+      <div className='nav-wrapper'>
+        <nav className='navigation'>
+          <a href='#text' className='item'>Text</a>
+          <a href='#twitter' className='item'>Twitter</a>
+          <a href='#carrousel' className='item'>Carrousel</a>
+          <a href='#share' className='item'>Share</a>
+        </nav>
+
+        <a id='menu' href='#' onClick={this.toggleMenu}>
+          <i className='fa fa-bars'></i>
+        </a>
+      </div>
     )
+  }
+
+  toggleMenu () {
+    // handle menu click
   }
 }
 
