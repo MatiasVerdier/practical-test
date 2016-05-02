82bed7f1d9f3203b824639be5d0c0886e007a6e5
Matias Verdier <verdier.matias@gmail.com>
2016-05-02 01:21:43 -0300

    add more text to TextBlock

--- app/components/TextBlock.jsx
+++ app/components/TextBlock.jsx
@@ -8,6 +8,7 @@ class TextBlock extends Component {
         <div className='content'>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam provident facilis tempora ut doloribus adipisci molestias, et commodi veritatis. Esse saepe illum excepturi vel, aliquam temporibus molestiae natus necessitatibus.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam provident facilis tempora ut doloribus adipisci molestias, et commodi veritatis. Esse saepe illum excepturi vel, aliquam temporibus molestiae natus necessitatibus.
           </p>
         </div>
       </div>
