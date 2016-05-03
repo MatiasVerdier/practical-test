424dbed504179b5dd4a8264c5df83684242d90fa
Matias Verdier <verdier.matias@gmail.com>
2016-05-03 00:20:19 -0300

    close navigation after click in nav links

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -4,16 +4,17 @@ class Navigation extends Component {
   constructor (props) {
     super(props)
     this.toggleMenu = this.toggleMenu.bind(this)
+    this.handleLinkClick = this.handleLinkClick.bind(this)
   }
 
   render () {
     return (
       <div className='nav-wrapper'>
         <nav ref='nav' className='navigation'>
-          <a href='#text' className='item'>Text</a>
-          <a href='#twitter' className='item'>Twitter</a>
-          <a href='#carrousel' className='item'>Carrousel</a>
-          <a href='#share' className='item'>Share</a>
+          <a href='#text' className='item' onClick={this.handleLinkClick}>Text</a>
+          <a href='#twitter' className='item' onClick={this.handleLinkClick}>Twitter</a>
+          <a href='#carrousel' className='item' onClick={this.handleLinkClick}>Carrousel</a>
+          <a href='#share' className='item' onClick={this.handleLinkClick}>Share</a>
         </nav>
 
         <a id='menu' href='#' onClick={this.toggleMenu}>
@@ -27,6 +28,10 @@ class Navigation extends Component {
     event.preventDefault()
     let nav = this.refs.nav
     nav.className = nav.className === 'navigation' ? 'navigation open' : 'navigation'
+  }
+
+  handleLinkClick () {
+    this.refs.nav.className = 'navigation'
   }
 }
 
