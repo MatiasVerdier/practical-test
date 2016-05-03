94cf5a9a1dd3747c74823a4d98f43e50ce682702
Matias Verdier <verdier.matias@gmail.com>
2016-05-03 17:43:43 -0300

    implementation of share block

--- app/components/ShareBlock.jsx
+++ app/components/ShareBlock.jsx
@@ -6,10 +6,24 @@ class ShareBlock extends Component {
       <div id='share' className='content-block'>
         <h2 className='title'>Share</h2>
         <div className='content'>
+          <div className='share-image'>
+            <img src={this.props.imageUrl} />
+
+            <div className='facebook-button'>
+              <div id='fb-root'></div>
+              <div className='fb-share-button' data-href={this.props.imageUrl} data-layout='button_count' data-mobile-iframe='true'>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
   }
+}
+
+ShareBlock.propTypes = { imageUrl: React.PropTypes.string }
+ShareBlock.defaultProps = {
+  imageUrl: 'http://www.planwallpaper.com/static/images/HD-Wallpapers1.jpeg'
 }
 
 export default ShareBlock
