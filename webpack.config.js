eb3ce0fc4e45134b9dfcab573925f282942a77e8
Matias Verdier <verdier.matias@gmail.com>
2016-05-05 21:43:29 -0300

    update webpack.config, split development and production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const webpack = require('webpack')
 const path = require('path')
 const autoprefixer = require('autoprefixer')
+const merge = require('webpack-merge')
+
+const TARGET = process.env.npm_lifecycle_event
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 }
 
-module.exports = {
+const common = {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
     PATHS.app
   ],
   output: {
@@ -25,31 +26,53 @@ module.exports = {
       {
         test: /\.js|jsx$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader']
+        loaders: ['babel-loader']
       },
       { test: /\.scss$/, loader: 'style-loader!css-loader!postcss-loader!sass-loader' },
       { test: /\.png$/, loader: 'url-loader?limit=85000' },
       { test: /\.jpg$/, loader: 'file-loader' }
     ]
-  },
-  postcss: function () {
-    return [autoprefixer]
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ],
-
-  devServer: {
-    contentBase: PATHS.build,
-    devTool: 'eval-source-map',
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    host: process.env.HOST,
-    port: process.env.PORT
   }
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/only-dev-server'
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          loaders: ['react-hot', 'babel-loader']
+        }
+      ]
+    },
+    postcss: function () {
+      return [autoprefixer]
+    },
+    devServer: {
+      contentBase: PATHS.build,
+      devTool: 'eval-source-map',
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      host: process.env.HOST,
+      port: process.env.PORT
+    }
+  })
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ]
+  })
 }
